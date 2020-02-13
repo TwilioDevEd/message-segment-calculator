8ee29603bc3e5a03274830d74e7cee729542a5cc
Mica Swyers <mswyers@twilio.com>
2020-02-13 11:25:11 -0800

    Detect when textbox input changes

---
+++ index.js
@@ -0,0 +1,13 @@
+function calculateLength() {
+  var messageBody = document.getElementById("message");
+
+  return 5;
+  // listener to update the length text
+}
+
+function updateMessageLength(length) {
+  // update the dom element
+  var messageLengthText = document.getElementById("message-length");
+
+  messageLengthText.appendChild = 5;
+}
