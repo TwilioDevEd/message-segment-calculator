2d1b9fc9c20c9b4d2d39357948fd9265e79b81c3
Samuel Mendes <smendes@twilio.com>
2021-03-29 20:51:45 +0200

    Fix UCS-2 size computing (#4)

--- segments_calculator.js
+++ segments_calculator.js
@@ -219,8 +219,6 @@ class GSM7EncodedChar extends EncodedChar {
 
 
 // Represent a UCS-2 encoded character
-// UCS-2 is of fixed length and requires 2 code units per character
-// a UCS-2 code unit is an octet (8bits)
 class UCS2EncodedChar extends EncodedChar {
   constructor(char, graphemeSize) {
     super(char);
@@ -229,16 +227,16 @@ class UCS2EncodedChar extends EncodedChar {
     if (char.length === 2) {
       this.codeUnits = [char.charCodeAt(0), char.charCodeAt(1)];
     } else {
-      this.codeUnits = [0x00, char.charCodeAt(0)];
+      this.codeUnits = [char.charCodeAt(0)];
     }
   }
 
   static codeUnitSizeInBits() {
-    return 8; // UCS-2 code units are 8bits long
+    return 16;
   }
 
   sizeInBits() {
-    return 16 * this.graphemeSize; // UCS-2 characters are always 2 code units -> 16bits
+    return 16 * this.raw.length;
   }
 }
 
