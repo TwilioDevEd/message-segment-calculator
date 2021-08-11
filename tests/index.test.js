1d972c42a32400a0243ea5957d6804f657e4cbf1
Giuseppe Verni <gverni@twilio.com>
2021-08-11 12:14:09 +0100

    Add test for GSM-7 escape characters

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,17 +1,19 @@
 const { SegmentedMessage } = require('../dist');
 
+const GSM7EscapeChars = ['|', '^', '€', '{', '}', '[', ']', '~', '\\']
+
 const TestData = [
   {
-    testDescription: 'GSM-7 with escape characters in one segment',
-    body: '{23456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789',
+    testDescription: 'GSM-7 in one segment',
+    body: '1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890',
     encoding: 'GSM-7',
     segments: 1,
     messageSize: 1120,
     totalSize: 1120,
   },
   {
-    testDescription: 'GSM-7 with escape characters in two segments',
-    body: '{234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890',
+    testDescription: 'GSM-7 in two segments',
+    body: '12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901',
     encoding: 'GSM-7',
     segments: 2,
     messageSize: 1127,
@@ -43,6 +45,25 @@ describe('Basic tests', () => {
       expect(segmentedMessage.segments.length).toBe(testMessage.segments);
       expect(segmentedMessage.messageSize).toBe(testMessage.messageSize);
       expect(segmentedMessage.totalSize).toBe(testMessage.totalSize);
+    });
+  });
+});
+
+describe('GSM-7 Escape Characters', () => {
+  GSM7EscapeChars.forEach((escapeChar) => {
+    test(`One segment with escape character ${escapeChar}`, () => {
+      const segmentedMessage = new SegmentedMessage(`${escapeChar}12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678`);
+      expect(segmentedMessage.encodingName).toBe('GSM-7');
+      expect(segmentedMessage.segments.length).toBe(1);
+      expect(segmentedMessage.messageSize).toBe(1120);
+      expect(segmentedMessage.totalSize).toBe(1120);
+    });
+    test(`Two segments with escape character ${escapeChar}`, () => {
+      const segmentedMessage = new SegmentedMessage(`${escapeChar}123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789`);
+      expect(segmentedMessage.encodingName).toBe('GSM-7');
+      expect(segmentedMessage.segments.length).toBe(2);
+      expect(segmentedMessage.messageSize).toBe(1127);
+      expect(segmentedMessage.totalSize).toBe(1223);
     });
   });
 });
