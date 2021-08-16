4c1e2687c5c669c8c89bf6ebfeb2a7c93eb7b5bb
Giuseppe Verni <gverni@twilio.com>
2021-08-16 19:48:31 +0100

    Refactor test cases in multiple files
    And add test for getNonGsmCharacters()

--- tests/index.test.js
+++ tests/index.test.js
@@ -144,39 +144,3 @@ describe('GSM-7 Escape Characters', () => {
     });
   });
 });
-
-describe('GSM-7 Segements analysis', () => {
-  const testMessage =
-    '1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567';
-  const segmentedMessage = new SegmentedMessage(testMessage);
-  test('Check User Data Header', () => {
-    for (var segmentIndex = 0; segmentIndex <= 2; segmentIndex++) {
-      for (var index = 0; index < 6; index++) {
-        expect(segmentedMessage.segments[segmentIndex][index].isUserDataHeader).toBe(true);
-      }
-    }
-  });
-
-  test('Check last segment has only 1 character', () => {
-    expect(segmentedMessage.segments[2].length).toBe(7);
-    expect(segmentedMessage.segments[2][6].raw).toBe('7');
-  });
-});
-
-describe('UCS-2 Segements analysis', () => {
-  const testMessage =
-    '😜2345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234';
-  const segmentedMessage = new SegmentedMessage(testMessage);
-  test('Check User Data Header', () => {
-    for (var segmentIndex = 0; segmentIndex <= 2; segmentIndex++) {
-      for (var index = 0; index < 6; index++) {
-        expect(segmentedMessage.segments[segmentIndex][index].isUserDataHeader).toBe(true);
-      }
-    }
-  });
-
-  test('Check last segment has only 1 character', () => {
-    expect(segmentedMessage.segments[2].length).toBe(7);
-    expect(segmentedMessage.segments[2][6].raw).toBe('4');
-  });
-});
